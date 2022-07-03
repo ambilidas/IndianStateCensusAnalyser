e3957cf5b89ff36c2ab1fbf3f7fe8c407b4e8ca5
Ambili das v s <ambili.kan@gmail.com>
2022-07-03 21:18:52 +0530

    Indian State Census Analyser Problem is created

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tWELCOME TO INDIAN STATE CENSUS ANALYSER PROBLEM");
+            Console.WriteLine("\t\t************************************************");
+            Console.ReadKey();
+        }
+    }
+}
