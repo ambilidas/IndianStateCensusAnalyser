de1376a2449a6688d105bd8745334dad3077944a
Ambili das v s <ambili.kan@gmail.com>
2022-07-05 12:44:26 +0530

    Ability to load info from csv file

---
+++ CensusAnalyserException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser
+{
+    public class CensusAnalyserException: Exception
+    {
+        public enum ExceptionType
+        {
+            FILE_NOT_FOUND,INVALID_FILE_TYPE,INCORRECT_HEADER,NO_SUCH_COUNTRY,INCORRECT_DELIMITER
+        }
+        public ExceptionType eType;
+
+        public CensusAnalyserException(string message,ExceptionType exceptionType) : base(message)
+        {
+            this.eType = exceptionType;
+        }
+    }
+}
